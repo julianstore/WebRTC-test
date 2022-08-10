2b6243bac695899ebcc70621433916a46d598b03
julianstore <today.julian.lim@gmail.com>
2022-08-09 21:43:18 -0700

    test cors

--- src/store/api-client/index.ts
+++ src/store/api-client/index.ts
@@ -9,9 +9,8 @@ const axiosInstance = axios.create({
     //Accept: 'application/json, text/plain, */*',
     'Content-Type': 'application/json', //multipart/form-data; boundary=----WebKitFormBoundaryBGVvH3OfggAP4Tad
     'Access-Control-Allow-Headers':
-      'access-control-allow-origin , access-control-allow-headers , access-control-allow-credentials',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true
+      'access-control-allow-origin , access-control-allow-headers',
+    'Access-Control-Allow-Origin': '*'
   }
 });
 
